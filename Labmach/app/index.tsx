21857e02dcd1a8b59a137ae7f5e3491c8bfcae86
abineet <abineet.bgr@gmail.com>
2025-02-18 15:08:49 +0100

    Added Home screen

---
+++ Labmach/app/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {ImageBackground, TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style = {{flex: 1}}>
+        <ImageBackground source={require('@/assets/images/homepage.png')} style={{flex:1, justifyContent: 'space-evenly'}}>
+            <Text style={styles.hometextintro}>
+                LaBmAcH is a platform 
+                which provides free access 
+                to information about 
+                latest research tools and 
+                techniques of different 
+                fields of science.
+            </Text>
+            <TouchableOpacity
+                style={styles.homebutton}>
+                <Text style={{color: "black", fontSize: 20, textAlign: "center"}}>Let{'\''}s Start</Text>
+            </TouchableOpacity>
+        </ImageBackground>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    hometextintro: {
+      color: "white",
+      fontSize: 30,
+      fontWeight: "bold",
+      textShadowOffset: {height:1,width:1},
+      textShadowRadius: 1,
+      textShadowColor: "black",
+      textAlign: "center"
+    },
+    homebutton: {
+      backgroundColor: "white",
+      borderRadius: 10,
+      borderWidth: 1,
+      height: 50,
+      width: 150,
+      alignSelf: 'center',
+      justifyContent: 'center'
+    }
+});
